02eaeb546b2ffc9f80e807b2c5dcac88577b1761
sihkang <sihkang@42seoul.student.co.kr>
2024-11-06 15:53:54 +0900

    fixed: order of glad and glfw

--- Dependency.cmake
+++ Dependency.cmake
@@ -41,6 +41,23 @@ ExternalProject_Add(
 set(DEP_LIST ${DEP_LIST} dep_spdlog)
 set(DEP_LIBS ${DEP_LIBS} spdlog)
 
+# glad
+ExternalProject_Add(
+	dep_glad
+	GIT_REPOSITORY "https://github.com/Dav1dde/glad"
+	GIT_TAG "v0.1.34"
+	GIT_SHALLOW 1
+	UPDATE_COMMAND ""
+	PATCH_COMMAND ""
+	CMAKE_ARGS
+		-DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+		-DGLAD_INSTALL=ON
+	TEST_COMMAND ""
+	)
+	
+set(DEP_LIST ${DEP_LIST} dep_glad)
+set(DEP_LIBS ${DEP_LIBS} glad)
+
 # glfw include
 ExternalProject_Add(
 	dep_glfw
@@ -59,22 +76,6 @@ ExternalProject_Add(
 set(DEP_LIST ${DEP_LIST} dep_glfw)
 set(DEP_LIBS ${DEP_LIBS} glfw3)
 
-# glad
-ExternalProject_Add(
-	dep_glad
-	GIT_REPOSITORY "https://github.com/Dav1dde/glad"
-	GIT_TAG "v0.1.34"
-	GIT_SHALLOW 1
-	UPDATE_COMMAND ""
-	PATCH_COMMAND ""
-	CMAKE_ARGS
-		-DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
-		-DGLAD_INSTALL=ON
-	TEST_COMMAND ""
-	)
-	
-set(DEP_LIST ${DEP_LIST} dep_glad)
-set(DEP_LIBS ${DEP_LIBS} glad)
 
 # stb
 ExternalProject_Add(
--- shader/transluscent.fs
+++ shader/transluscent.fs
@@ -10,6 +10,25 @@ float sphereSDF(vec3 pos, vec3 center, float radius) {
     return length(pos - center) - radius;
 }
 
+vec3 spheres[3] = vec3[](vec3(0.0, 0.0, 0.0), vec3(2.0, 1.0, -3.0), vec3(-1.5, -0.5, 2.0));
+vec3 colors[3] = vec3[](vec3(0.7, 0.2, 0.2), vec3(0.2,0.7,0.2), vec3(0.2,0.2,0.7));
+float radii[3] = float[](1.0, 0.5, 0.8);
+
+int idx;
+
+float minSphereSDF(vec3 pos) {
+    float minDist = 1e10; // 초기값을 큰 값으로 설정
+    for (int i = 0; i < 3; i++) {
+        float d = sphereSDF(pos, spheres[i], radii[i]);
+		if (minDist > d)
+		{
+			minDist = d;
+			idx = i;
+		}
+    }
+    return minDist;
+}
+
 float raymarcher(in vec3 rayOrigin, in vec3 rayDir) {
     const float maxd = 50.0;
     const float eps = 0.01;
@@ -17,10 +36,10 @@ float raymarcher(in vec3 rayOrigin, in vec3 rayDir) {
 
     for (int i = 0; i < 100; i++) {
         vec3 currentPos = rayOrigin + rayDir * dist;
-        float h = sphereSDF(currentPos, vec3(0.0), 1.0); // 중심이 원점, 반지름이 1인 구
-        if (h < eps) break; // 구에 도달하면 종료
+        float h = minSphereSDF(currentPos); // 최소 SDF 사용
+        if (h < eps) break; // 오브젝트에 도달하면 종료
         dist += h;
-        if (dist > maxd) return -1.0; // 최대 거리를 넘으면 없다고 판단
+        if (dist > maxd) return -1.0; // 최대 거리를 넘으면 없음
     }
     return dist;
 }
@@ -34,16 +53,16 @@ vec3 normal( in vec3 pos )
     const vec3 v3 = vec3(-1.0, 1.0,-1.0);
     const vec3 v4 = vec3( 1.0, 1.0, 1.0);
 
-	return -normalize( v1*sphereSDF( pos + v1*eps, vec3(0.0), 1.0 ) + 
-					  v2*sphereSDF( pos + v2*eps, vec3(0.0), 1.0 ) + 
-					  v3*sphereSDF( pos + v3*eps, vec3(0.0), 1.0 ) + 
-					  v4*sphereSDF( pos + v4*eps, vec3(0.0), 1.0 ) );
+	return -normalize( v1*sphereSDF( pos + v1*eps, spheres[idx], radii[idx] ) + 
+					  v2*sphereSDF( pos + v2*eps, spheres[idx], radii[idx] ) + 
+					  v3*sphereSDF( pos + v3*eps, spheres[idx], radii[idx] ) + 
+					  v4*sphereSDF( pos + v4*eps, spheres[idx], radii[idx] ) );
 }
 
 vec3 lightingModel( in vec3 lightdir, in vec3 lightcol, in vec3 albedo, in vec3 norm, in vec3 raydir)
 {
 	float diffuse = dot(norm, -lightdir);
-	float specular = pow(max(dot(-raydir, reflect(lightdir, norm)), -0.0), 8.0);
+	float specular = pow(max(dot(-raydir, reflect(lightdir, norm)), -0.0), 128.0);
 
 	return lightcol * (albedo * diffuse + specular);
 }
@@ -52,10 +71,10 @@ vec3 material(in vec3 contactPoint, in vec3 raydir)
 {
 	vec3 norm = - normal(contactPoint);
 
-	vec3 lightDir = -normalize(vec3(5.0, 10.0, 20.0));
+	vec3 lightDir = -normalize(vec3(1.0, 1.0, 1.0));
 	vec3 lightCol = vec3(1.0, 1.0, 1.0);
 
-	vec3 albedo = vec3(0.72, 0.28, 0.2);
+	vec3 albedo = colors[idx];
 
 	float amb = 0.2;
 	vec3 pointColor = lightingModel(lightDir, lightCol, albedo, norm, raydir);
